69ca3f147acb95777b8ce9fb1db8ec81f0172165
BalliAsghar <ballihacker@outlook.com>
2022-02-28 22:06:37 +0000

    every ten min

---
+++ index.js
@@ -0,0 +1,26 @@
+const schedule = require("node-schedule");
+const fs = require("fs/promises");
+const execa = require("execa");
+
+// schedule a job to run every 10 minutes
+
+schedule.scheduleJob("*/10 * * * *", async () => {
+  try {
+    // read key from ./key.txt
+
+    // random text
+    const message = `${Math.random()}`;
+
+    // write the message to readme.md
+    await fs.writeFile("./README.md", message);
+
+    // commit the change
+    await execa("git", ["add", "."]);
+    await execa("git", ["commit", "-m", message]);
+    await execa("git", ["push"]);
+
+    console.log(`Successfully wrote commit message: ${message}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
